66eff7808bfc7ea2c6fee387b7034cde21e850d1
MatiBlack7 <matiasleandrodeleonamador@gmail.com>
2025-10-05 18:46:52 -0300

    Delete backend/Koi Dataset.bat

--- backend/Koi Dataset.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -L -o "%~dp0koi.csv" "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=koi&format=csv"
-echo Listo, archivo koi.csv descargado en %~dp0
-pause
